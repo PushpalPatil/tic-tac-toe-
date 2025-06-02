87c4c59632e9f3fb8d3c73482379cd072200a5c5
Pushpal Patil <gauripushp@gmail.com>
2025-06-02 17:53:54 -0400

    pr 2

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -1,24 +1,59 @@
 import { useState } from 'react'
-
 import './App.css'
-import './game/game'
+import './gameAlgo/game'
+import { gameState } from './gameAlgo/game'
 
 function App() {
   
 
   const [game, setGame] = useState(gameState())
+  // let [player, setPlayer] = useState('')
 
+  // function assignPlayerX(){
+  //   setPlayer = 'X'
+  // }
   return (
     <>
-    <div className='App'> </div>
+    <div className='app'> </div>
       <div className='gameContainer'>
-        <h1 className='title'>Welcome to TicTacToe</h1>
-        <h2 className='currentPlayer'>
-          Current Player: <span className={`player-indicator player-${game.currentPlayer}`} /> 
-        </h2>
+        <h1 className='title'>Welcome to TicTacToe </h1>
 
+        <div className='info'>
 
-        
+          <div className='currentPlayer'>
+            Current Player: <span className={`player-indicator player-${game.currentPlayer}`}>{game.currentPlayer.toUpperCase()}</span>
+          </div>
+          {/* <div>
+            <button style = {{marginRight:'10px', marginTop: '1rem'}} onClick={assignPlayerX}> X</button>
+            <button style = {{marginLeft:'10px', marginTop: '1rem'}}> O </button>
+
+          </div> */}
+
+            
+          
+          {game.endState && (
+            <div className='gameResult'>
+              {game.endState === 'Tie' ? 'TIE' : `Player ${game.endState.toUpperCase()} wins!`}
+            </div>
+          )}
+        </div>
+        <div className='board'>
+          {game.board.map((row, rowIndex) =>
+            row.map((cell, colIndex) =>(
+              <div
+                key = {`${rowIndex} - ${colIndex}`}
+                className= {`cell ${cell ? `cell-${cell}` : ''}`}
+                onClick={() => {
+                  setGame(prev => move(prev, {row: rowIndex, col: colIndex}))
+                }}
+              > 
+                {cell ? cell.toUpperCase():''}
+              </div>
+            ))
+          
+          )}
+
+        </div>
       </div>
     </>
   )

9c89cacd294e13ab28ef47e995a38a05214b5050
Pushpal Patil <gauripushp@gmail.com>
2025-06-04 15:24:44 -0400

    api added

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -12,29 +12,46 @@ function App() {
     setGame(prev => move(prev, i))
   }
 
+ 
+ 
+  const isTie = (): boolean => {
+    return game.endState === 'Tie';
+  };
   return(
-    <div className='gameboard'> 
+    <>
+    <div className='wrap'>
+      <h1 className='title'> Welcome to TicTacToe! </h1>
+    
+    
+      <div className='gameboard'> 
 
-      <h1> Welcome to TicTacToe! </h1>
-      <div className='row'>
-        <div onClick={ () => clickCell(0)} className='cell'>{game.board[0]}</div>
-        <div onClick={ () => clickCell(1)} className='cell'>{game.board[1]}</div>
-        <div onClick={ () => clickCell(2)} className='cell'>{game.board[2]}</div>
-      </div>
-      <div className="row">
-        <div onClick={() => clickCell(3)} className="cell">{game.board[3]}</div>
-        <div onClick={() => clickCell(4)} className="cell">{game.board[4]}</div>
-        <div onClick={() => clickCell(5)} className="cell">{game.board[5]}</div>
-      </div>
-      <div className="row">
-        <div onClick={() => clickCell(6)} className="cell">{game.board[6]}</div>
-        <div onClick={() => clickCell(7)} className="cell">{game.board[7]}</div>
-        <div onClick={() => clickCell(8)} className="cell">{game.board[8]}</div>
-      </div>
-      {game.endState && <div className='result'>{game.endState} wins!</div>}
-    
-    
-    </div> 
+        <div className='row'>
+          <div onClick={ () => clickCell(0)} className='cell'>{game.board[0]}</div>
+          <div onClick={ () => clickCell(1)} className='cell'>{game.board[1]}</div>
+          <div onClick={ () => clickCell(2)} className='cell'>{game.board[2]}</div>
+        </div>
+        <div className="row">
+          <div onClick={() => clickCell(3)} className="cell">{game.board[3]}</div>
+          <div onClick={() => clickCell(4)} className="cell">{game.board[4]}</div>
+          <div onClick={() => clickCell(5)} className="cell">{game.board[5]}</div>
+        </div>
+        <div className="row">
+          <div onClick={() => clickCell(6)} className="cell">{game.board[6]}</div>
+          <div onClick={() => clickCell(7)} className="cell">{game.board[7]}</div>
+          <div onClick={() => clickCell(8)} className="cell">{game.board[8]}</div>
+        </div>
+        
+        {
+        game.endState && 
+        <div  className='result'>
+          {isTie() ? 'Tie!' : `${game.endState} Wins!`} 
+        </div>
+        }
+        
+        
+      </div> 
+    </div>
+    </>
   )
 
 }
